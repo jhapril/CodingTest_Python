0b57c0cd60bf21279989fa40e3fd5f8bca598e2e
jhapril <147474497+jhapril@users.noreply.github.com>
2023-11-27 19:04:30 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,6 +1,6 @@
 def solution(my_string, overwrite_string, s):
     s = int(s)
     # answer = my_string.replace(my_string[s:s+len(overwrite_string)],
-    #                            overwrite_string)
+                               # overwrite_string)
     answer = my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
     return answer
