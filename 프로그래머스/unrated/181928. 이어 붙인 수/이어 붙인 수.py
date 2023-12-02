b1304d0fc08f0f9fd0c39ff48ef71b1b2f70e5be
jhapril <147474497+jhapril@users.noreply.github.com>
2023-12-02 12:52:57 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,11 +1,18 @@
 def solution(num_list):
-    answer1 = ''
-    answer2 = ''
-    for i in range(len(num_list)):
-        if num_list[i] % 2 == 1:
-            answer1 += str(num_list[i])
-        elif num_list[i] % 2 == 0:
-            answer2 += str(num_list[i])
-    answer1 = int(answer1)
-    answer2 = int(answer2)
-    return answer1+answer2
+    answer1 , answer2 = '',''
+    
+    # for i in range(len(num_list)):
+    #     if num_list[i] % 2 == 1:
+    #         answer1 += str(num_list[i])
+    #     elif num_list[i] % 2 == 0:
+    #         answer2 += str(num_list[i])
+    # answer1 = int(answer1)
+    # answer2 = int(answer2)
+    # return answer1+answer2
+    
+    for i in num_list:
+        if i % 2 == 1:
+            answer1+=str(i)
+        elif i % 2 == 0:
+            answer2+=str(i)
+    return int(answer1)+int(answer2)
