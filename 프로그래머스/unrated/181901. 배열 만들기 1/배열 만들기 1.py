c4223b165afe1953be9cbc69411e4ceef31547b6
jhapril <147474497+jhapril@users.noreply.github.com>
2023-12-21 16:40:55 +0900

    [level unrated] Title: 배열 만들기 1, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,6 +1,8 @@
 def solution(n, k):
-    answer = []
-    for num in range(1,n+1):
-        if num % k == 0:
-            answer.append(num)
-    return answer
+    
+    # for num in range(1,n+1):
+    #     if num % k == 0:
+    #         answer.append(num)
+    # return answer
+    
+    return [num for num in range(k, n+1, k)]
